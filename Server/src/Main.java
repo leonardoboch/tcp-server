585ee1cec2a3a8b86eeeebbccee74a6549e7d955
Leonardo Bochnia <bochnialeonardo@gmail.com>
2023-09-16 17:43:40 -0300

    Added some logs to server

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,10 +8,16 @@ public class Main {
         int port = 12345;
         System.out.println("Starting server...");
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Servidor TCP aguardando conexões na porta");
+            InetAddress inet = serverSocket.getInetAddress();
+            System.out.println("Host Address:: " + inet.getHostAddress());
+            System.out.println("Host Name:: " + inet.getHostName());
+            System.out.println("Servidor TCP aguardando conexões na porta:: " + serverSocket.getLocalPort());
             while(true) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("Cliente conectado:: " + clientSocket.getInetAddress());
+                InetAddress inetClient = clientSocket.getInetAddress();
+                System.out.println("Cliente conectado:: " + inetClient);
+                System.out.println(inetClient.getHostAddress());
+                System.out.println(inetClient.getHostName());
                 Thread clientThread = new Thread(new ClientHandler(clientSocket));
                 clientThread.start();
             }
